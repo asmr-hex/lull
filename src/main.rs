ca84ac51620d183f4cf23435f144ac0c5dad854b
connorwalsh <c@polygon.pizza>
2018-12-04 22:30:49 -0500

    [cw,dd] idk, ust riffin
    Co-authored-by: Lydia Ding <lydiding@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
+    // initialize in memory wave buffers
+
+    // begin timed loop to play the sound
     println!("Hello, world!");
+}
+
+fn init_buffers() -> [] {
+    let n i32 = 1024;
+    // we must use a vector
 }
